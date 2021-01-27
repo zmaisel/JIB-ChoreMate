e87622de59aad1131d0a8bc4a12b1b77d211e49b
abby-rose <54951866+abby-rose@users.noreply.github.com>
2021-01-26 19:44:37 -0500

    a lil chore functionality

--- choremate/lib/main.dart
+++ choremate/lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
       routes: {
         // When navigating to the "/" route, build the FirstScreen widget.
         '/Chores': (context) => Chores(),
+        '/Home': (context) => Home(),
         // When navigating to the "/second" route, build the SecondScreen widget.
         //add next route here
       },
